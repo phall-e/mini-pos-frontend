bf6e8e9aabbe6a951bba4f6d8c10f27a3d0c2d72
EOM Phall <123912861+phall-e@users.noreply.github.com>
2026-06-10 11:06:03 +0700

    ft: add authorization in stock in module

--- pages/admin/stocking/stock-in/index.vue
+++ pages/admin/stocking/stock-in/index.vue
@@ -20,7 +20,7 @@
               <Icon name="solar:magnifer-outline" />
             </template>
           </el-input>
-          <el-button type="primary" @click="openCreateDialog">
+          <el-button v-if="can('stock-in-create')" type="primary" @click="openCreateDialog">
             <Icon name="solar:add-circle-outline" size="18" />
             <span class="ml-1">{{ t('create') }}</span>
           </el-button>
@@ -53,12 +53,12 @@
         <el-table-column :label="t('columns.action')" fixed="right" width="140" align="center">
           <template #default="{ row }">
             <div class="flex items-center justify-center gap-2">
-              <el-tooltip :content="t('edit')" placement="top">
+              <el-tooltip v-if="can('stock-in-edit')" :content="t('edit')" placement="top">
                 <el-button type="success" circle @click="openEditDialog(row)">
                   <Icon name="solar:pen-2-outline" size="17" />
                 </el-button>
               </el-tooltip>
-              <el-tooltip :content="t('delete')" placement="top">
+              <el-tooltip v-if="can('stock-in-delete')" :content="t('delete')" placement="top">
                 <el-button type="danger" circle @click="deleteItem(row)">
                   <Icon name="solar:trash-bin-trash-outline" size="17" />
                 </el-button>
@@ -240,6 +240,16 @@
 <script setup lang="ts">
 import type { FormInstance, FormRules } from 'element-plus'
 import DropZone from '~/@core/components/DropZone.vue'
+
+definePageMeta({
+  permission: 'stock-in-read'
+})
+
+useHead({
+  title: 'MINI-POS-STOCK-IN'
+})
+
+const { can } = usePermission();
 
 interface SelectOption {
   id: number

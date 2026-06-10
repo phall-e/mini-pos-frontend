0e3624163e45560d78e503fcbb240bd4fd867f7d
EOM Phall <123912861+phall-e@users.noreply.github.com>
2026-06-10 10:57:49 +0700

    ft: add authorization in customer module

--- pages/admin/master-data/customer/index.vue
+++ pages/admin/master-data/customer/index.vue
@@ -20,7 +20,7 @@
               <Icon name="solar:magnifer-outline" />
             </template>
           </el-input>
-          <el-button type="primary" @click="openCreateDialog">
+          <el-button v-if="can('customer-create')" type="primary" @click="openCreateDialog">
             <Icon name="solar:add-circle-outline" size="18" />
             <span class="ml-1">{{ t('create') }}</span>
           </el-button>
@@ -71,12 +71,12 @@
         <el-table-column :label="t('columns.action')" fixed="right" width="140" align="center">
           <template #default="{ row }">
             <div class="flex items-center justify-center gap-2">
-              <el-tooltip :content="t('edit')" placement="top">
+              <el-tooltip v-if="can('customer-edit')" :content="t('edit')" placement="top">
                 <el-button type="success" circle @click="openEditDialog(row)">
                   <Icon name="solar:pen-2-outline" size="17" />
                 </el-button>
               </el-tooltip>
-              <el-tooltip :content="t('delete')" placement="top">
+              <el-tooltip v-if="can('customer-delete')" :content="t('delete')" placement="top">
                 <el-button type="danger" circle @click="deleteItem(row)">
                   <Icon name="solar:trash-bin-trash-outline" size="17" />
                 </el-button>
@@ -225,6 +225,16 @@
 import type { FormInstance, FormRules } from 'element-plus'
 import DropZone from '~/@core/components/DropZone.vue'
 import SingleUpload from '~/@core/components/SingleUpload.vue'
+
+definePageMeta({
+  permission: 'customer-read'
+})
+
+useHead({
+  title: 'MINI-POS-CUSTOMER'
+})
+
+const { can } = usePermission();
 
 type Gender = 'male' | 'female'
 
